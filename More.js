69e36bb2d50761c3cb8559ea7e3f30863a771d4d
Pete Steere <psteere.428@gmail.com>
2024-06-07 15:24:06 -0400

    update repo

---
+++ More.js
@@ -0,0 +1,15 @@
+
+
+
+var peteArray = ["String to be shifted(removed)",""];
+
+//push defines new values to array
+//pop removes from END of array
+//shift removes values in order
+//unshift adds values at the begining of array
+
+
+const peteVar = 4;
+
+
+
